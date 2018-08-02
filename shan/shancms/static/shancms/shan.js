45dddd89161fb47ee22acf50aacaf68f90ce6cc2
Gabriel Vieira <gluisvieira@gmail.com>
2018-08-02 18:33:35 -0300

    Show events in dashboard

--- shan/shancms/static/shancms/shan.js
+++ shan/shancms/static/shancms/shan.js
@@ -1,4 +1,4 @@
-console.log('Welcome to Shelf Analytics.');
+if (typeof moment === 'undefined') { console.error('Required lib "moment.js" not found.'); }
 
 function getEvents(shelfId, callback) {
   $.get('/shancms/events', {'shelf_id': shelfId})
@@ -10,40 +10,49 @@ function getEvents(shelfId, callback) {
     });
 }
 
-function showChart() {
+function showChart(chartData) {
   var ctx = document.getElementById("myChart").getContext('2d');
   var myChart = new Chart(ctx, {
-      type: 'bar',
+      type: 'line',
       data: {
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-          datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
+          labels: chartData.labels,
+          datasets: [
+            {
+              label: 'Walked',
+              backgroundColor: 'rgba(255, 0, 0)',
+              borderColor: 'rgba(255, 0, 0)',
+              data: chartData.walkedData,
+              fill: false
+            },
+            {
+              label: 'Pondered',
+              backgroundColor: 'rgba(0, 255, 0)',
+              borderColor: 'rgba(0, 255, 0)',
+              data: chartData.ponderedData,
+              fill: false
+            },
+            {
+              label: 'Pondered Duration',
+              backgroundColor: 'rgba(255, 255, 0)',
+              borderColor: 'rgba(255, 255, 0)',
+              data: chartData.ponderedDurationData,
+              fill: false
+            },
+            {
+              label: 'Interacted',
+              backgroundColor: 'rgba(0, 0, 255)',
+              borderColor: 'rgba(0, 0, 255)',
+              data: chartData.interactedData,
+              fill: false
+            }
+          ]
       },
       options: {
-          responsive: false,
+          responsive: true,
           scales: {
               yAxes: [{
                   ticks: {
-                      beginAtZero:true
+                      beginAtZero: true
                   }
               }]
           }
@@ -51,10 +60,88 @@ function showChart() {
   });
 }
 
+function momentToDate(m) {
+  return new Date(m.year(), m.month(), m.date());
+}
+
+function dateToString(date) {
+  return moment(date).format('MMM D YYYY');
+}
+
+function aggregateEventsByDateString(events) {
+  if (events.length === 0) {
+    throw new DOMException('array "events" is empty');
+  }
+  var eventsByDateString = {};
+  for (var i = 0; i < events.length; i++) {
+    var dateString = dateToString(events[i].date);
+    if (!eventsByDateString[dateString]) {
+      eventsByDateString[dateString] = [];
+    }
+    eventsByDateString[dateString].push({type: events[i].type, params: events[i].params});
+  }
+  return eventsByDateString;
+}
+
+function daysBetween(date1, date2) {
+  var ONE_DAY_MS = 1000 * 60 * 60 * 24;
+  var deltaMs = date2.getTime() - date1.getTime();
+  return Math.ceil(deltaMs / ONE_DAY_MS);
+}
+
+function average(values) {
+  var sum = values.reduce(function (acc, val) { return acc + val; }, 0);
+  return sum / values.length;
+}
+
+function buildChartData(firstDate, lastDate, eventsByDateString) {
+  var firstMoment = moment(firstDate);
+  var currDate = momentToDate(firstMoment);
+  var lastMoment = moment(lastDate);
+  var lastDate = momentToDate(lastMoment);
+  var labels = [];
+  var walkedData = [];
+  var interactedData = [];
+  var ponderedData = [];
+  var ponderedDurationData = [];
+  while (currDate.getTime() <= lastDate.getTime()) {
+    var currDateString = dateToString(currDate);
+    var events = eventsByDateString[currDateString];
+    if (!events) { events = []; }
+    labels.push(dateToString(currDate));
+    walkedData.push(events.filter(function (e) { return e.type === 'walked'; }).length);
+    interactedData.push(events.filter(function (e) { return e.type === 'interacted'; }).length);
+    ponderedData.push(events.filter(function (e) { return e.type === 'pondered'; }).length);
+    var ponderedEvents = events.filter(function (e) { return e.type === 'pondered'});
+    if (ponderedEvents.length > 0) {
+      var durationsSec = ponderedEvents.map(function (e) { return e.params['duration'] / 1000; });
+      ponderedDurationData.push(Math.round(average(durationsSec)));
+    } else {
+      ponderedDurationData.push(null);
+    }
+    var nextMoment = moment(currDate).add(1, 'days');
+    currDate = momentToDate(nextMoment);
+  }
+  return {
+    labels: labels,
+    walkedData: walkedData, 
+    interactedData: interactedData, 
+    ponderedData: ponderedData, 
+    ponderedDurationData: ponderedDurationData
+  };
+}
+
+window.moment = moment;
+window.aggregateEventsByDateString = aggregateEventsByDateString;
+window.buildChartData = buildChartData;
+
 getEvents(1, function (data) {
   if (data) {
-    showChart();
     console.log(data);
+    window.data = data;
+    var eventsByDateString = aggregateEventsByDateString(data.events);
+    var chartData = buildChartData(data.events[0].date, data.events[data.events.length - 1].date, eventsByDateString);
+    showChart(chartData);
   } else {
     alert('Error getting events.');
   }
--- shan/shancms/views.py
+++ shan/shancms/views.py
@@ -14,7 +14,7 @@ def get_events(request):
     if shelf_id_str is None:
         return JsonResponse({'events': []})
     shelf_id = int(shelf_id_str)
-    events = Event.objects.filter(shelf_id=shelf_id)
+    events = Event.objects.filter(shelf_id=shelf_id).order_by('creation_date')
     evt_objs = []
     for evt in events:
         obj = {'type': evt.event_type, 'date': evt.creation_date, 'params': json.loads(evt.event_params)}

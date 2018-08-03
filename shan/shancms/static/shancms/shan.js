6d751657ce8cb4ec847e38bbde1dd3699f2d3ed5
Gabriel Vieira <gluisvieira@gmail.com>
2018-08-03 11:24:36 -0300

    Show events by day,week,month

--- shan/shancms/static/shancms/shan.js
+++ shan/shancms/static/shancms/shan.js
@@ -10,36 +10,51 @@ function getEvents(shelfId, callback) {
     });
 }
 
-function showChart(chartData) {
-  var ctx = document.getElementById("myChart").getContext('2d');
+var CHART_LABEL_WALKED = 'Walked';
+var CHART_LABEL_INTERACTED = 'Interacted';
+var CHART_LABEL_PONDERED = 'Pondered';
+var CHART_LABEL_PONDERED_DURATION = 'Pondered Duration';
+
+function createAndShowChart(chartData) {
+  console.log('Showing chart with data:', chartData);
+  /*
+  var container = document.getElementById('chart-container');
+  if (document.getElementById('shan-chart')) {
+    container.removeChild(document.getElementById('shan-chart'));
+  }
+  var canvas = document.createElement('canvas');
+  canvas.setAttribute('id', 'shan-chart');
+  container.appendChild(canvas);
+  */
+  var ctx = document.getElementById('shan-chart').getContext('2d');
   var myChart = new Chart(ctx, {
       type: 'line',
       data: {
           labels: chartData.labels,
           datasets: [
             {
-              label: 'Walked',
+              label: CHART_LABEL_WALKED,
               backgroundColor: 'rgba(255, 0, 0)',
               borderColor: 'rgba(255, 0, 0)',
               data: chartData.walkedData,
               fill: false
             },
             {
-              label: 'Pondered',
+              label: CHART_LABEL_PONDERED,
               backgroundColor: 'rgba(0, 255, 0)',
               borderColor: 'rgba(0, 255, 0)',
               data: chartData.ponderedData,
               fill: false
             },
             {
-              label: 'Pondered Duration',
+              label: CHART_LABEL_PONDERED_DURATION,
               backgroundColor: 'rgba(255, 255, 0)',
               borderColor: 'rgba(255, 255, 0)',
               data: chartData.ponderedDurationData,
               fill: false
             },
             {
-              label: 'Interacted',
+              label: CHART_LABEL_INTERACTED,
               backgroundColor: 'rgba(0, 0, 255)',
               borderColor: 'rgba(0, 0, 255)',
               data: chartData.interactedData,
@@ -58,6 +73,7 @@ function showChart(chartData) {
           }
       }
   });
+  return myChart;
 }
 
 function momentToDate(m) {
@@ -65,7 +81,7 @@ function momentToDate(m) {
 }
 
 function dateToString(date) {
-  return moment(date).format('MMM D YYYY');
+  return moment(date).format('MMM D');
 }
 
 function aggregateEventsByDateString(events) {
@@ -131,18 +147,137 @@ function buildChartData(firstDate, lastDate, eventsByDateString) {
   };
 }
 
-window.moment = moment;
-window.aggregateEventsByDateString = aggregateEventsByDateString;
-window.buildChartData = buildChartData;
+function updateChart(chart, chartData) {
+  chart.data.labels = [];
+  chart.data.datasets = [];
+  chart.update();
+  chart.data.labels = chartData.labels;
+  chart.data.datasets.push({
+    label: CHART_LABEL_WALKED,
+    backgroundColor: 'rgba(255, 0, 0)',
+    borderColor: 'rgba(255, 0, 0)',
+    data: chartData.walkedData,
+    fill: false
+  });
+  chart.data.datasets.push({
+    label: CHART_LABEL_PONDERED,
+    backgroundColor: 'rgba(0, 255, 0)',
+    borderColor: 'rgba(0, 255, 0)',
+    data: chartData.ponderedData,
+    fill: false
+  });
+  chart.data.datasets.push({
+    label: CHART_LABEL_PONDERED_DURATION,
+    backgroundColor: 'rgba(255, 255, 0)',
+    borderColor: 'rgba(255, 255, 0)',
+    data: chartData.ponderedDurationData,
+    fill: false
+  });
+  chart.data.datasets.push({
+    label: CHART_LABEL_INTERACTED,
+    backgroundColor: 'rgba(0, 0, 255)',
+    borderColor: 'rgba(0, 0, 255)',
+    data: chartData.interactedData,
+    fill: false
+  });
+  chart.update();
+  console.log('Chart updated with:', chartData);
+}
 
-getEvents(1, function (data) {
-  if (data) {
-    console.log(data);
-    window.data = data;
-    var eventsByDateString = aggregateEventsByDateString(data.events);
-    var chartData = buildChartData(data.events[0].date, data.events[data.events.length - 1].date, eventsByDateString);
-    showChart(chartData);
+function aggregateChartData(chartData, period) { // TODO: refactor this hacky crap
+  var newData = {
+    labels: [],
+    walkedData: [],
+    interactedData: [],
+    ponderedData: [],
+    ponderedDurationData: []
+  };
+  var daysInPeriod;
+  if (period === 'days') { 
+    return chartData; 
+  } else if (period === 'weeks') {
+    daysInPeriod = 7;
+  } else if (period === 'months') {
+    daysInPeriod = 30;
   } else {
-    alert('Error getting events.');
+    throw new DOMException('invalid period "' + period + '"')
   }
-})
+  var index;
+  var newIndex = 0;
+  for (index = 0; index < (chartData.labels.length - daysInPeriod); index += daysInPeriod) {
+    newData.labels[newIndex] = chartData.labels[index] + ' to ' + chartData.labels[index + daysInPeriod - 1];
+    var walked = [];
+    var pondered = [];
+    var ponderedDuration = [];
+    var interacted = [];
+    for (var i = 0; i < daysInPeriod; i++) {
+      walked.push(chartData.walkedData[index + i]);
+      pondered.push(chartData.ponderedData[index + i]);
+      ponderedDuration.push(chartData.ponderedDurationData[index + i]);
+      interacted.push(chartData.interactedData[index + i]);
+    }
+    newData.walkedData[newIndex] = Math.round(average(walked));
+    newData.ponderedData[newIndex] = Math.round(average(pondered));
+    newData.ponderedDurationData[newIndex] = Math.round(average(ponderedDuration));
+    newData.interactedData[newIndex] = Math.round(average(interacted));
+    newIndex += 1;
+  }
+  // rest of the data up until now
+  if (index < chartData.labels.length) {
+    newData.labels[newIndex] = chartData.labels[index] + ' to '
+    var walked = [];
+    var pondered = [];
+    var ponderedDuration = [];
+    var interacted = [];
+    while (index < chartData.labels.length) {
+      walked.push(chartData.walkedData[index]);
+      pondered.push(chartData.ponderedData[index]);
+      ponderedDuration.push(chartData.ponderedDurationData[index]);
+      interacted.push(chartData.interactedData[index]);
+      index += 1;
+    }
+    newData.labels[newIndex] += chartData.labels[index - 1]
+    newData.walkedData[newIndex] = Math.round(average(walked));
+    newData.ponderedData[newIndex] = Math.round(average(pondered));
+    newData.ponderedDurationData[newIndex] = Math.round(average(ponderedDuration));
+    newData.interactedData[newIndex] = Math.round(average(interacted));
+  }
+  return newData;
+}
+
+$(document).ready(function () {
+  initialize();
+});
+
+function initialize() {
+  window.moment = moment;
+  window.aggregateEventsByDateString = aggregateEventsByDateString;
+  window.buildChartData = buildChartData;
+
+  var shelfId = 1;
+
+  getEvents(shelfId, function (data) {
+    if (data) {
+      console.log(data);
+      window.data = data;
+      var eventsByDateString = aggregateEventsByDateString(data.events);
+      var chartData = buildChartData(data.events[0].date, data.events[data.events.length - 1].date, eventsByDateString);
+      var chart = createAndShowChart(chartData);
+      $('#agg-fieldset').show();
+      $('#agg-days').on('change', function () {
+        var updatedChartData = aggregateChartData(chartData, 'days');
+        updateChart(chart, updatedChartData);
+      });
+      $('#agg-weeks').on('click', function () {
+        var updatedChartData = aggregateChartData(chartData, 'weeks');
+        updateChart(chart, updatedChartData);
+      });
+      $('#agg-months').on('click', function () {
+        var updatedChartData = aggregateChartData(chartData, 'months');
+        updateChart(chart, updatedChartData);
+      });
+    } else {
+      alert('Error getting events.');
+    }
+  })
+}
